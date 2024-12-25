6c224e6769b5fb9779de26ef2ce8f0f43d74152d
AyushNotiyal <ayushnotiyal12@gmail.com>
2024-12-26 02:03:53 +0530

    Updated program1.cpp with a sum function

--- program1.cpp
+++ program1.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
 int main(){
-    std::cout<<"Hello,World!";
+    int num1,num2,sum;
+    std::cout<<"Value:";
+    std::cin>>num1;
+    std::cout<<"value";
+    std::cin>>num2;
+    sum=num1+num2;
+    std::cout<<"Sum="<<sum;
+
 }
